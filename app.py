a2bbbf1ad2c772a4c9d11e52b07d20292ccee3c5
Issac Kim <ikim1@stuy.edu>
2016-10-04 22:01:46 -0400

    edit to test POST request form

--- app.py
+++ app.py
@@ -1,23 +1,15 @@
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
-user = "issac"
-password = "kim"
 
 @app.route("/")
 def forms():
     return render_template("main.html")
 
 @app.route("/authenticate/", methods = ['POST'])
-def auth():
+def do_things():
     d = request.form
-    if d['pass'] == password:
-        if d['user'] == user:
-            return "login successful"
-        else:
-            return "problem with login"
-    else:
-        return "problem with login"
+    return d
 if __name__ == '__main__':
     app.debug = True
     app.run()

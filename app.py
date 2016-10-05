1f9299ef54c997e519b7ad13750cce1b6e3da337
Issac-Kim <ikim1@stuy.edu>
2016-10-04 23:24:15 -0400

    edit app

--- app.py
+++ app.py
@@ -1,15 +1,25 @@
 from flask import Flask, render_template, request
-
+from utils import readCSV
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/login/")
 def forms():
     return render_template("main.html")
 
+@app.route("/register/")
+def reg():
+    return render_template("register.html")
+
 @app.route("/authenticate/", methods = ['POST'])
-def do_things():
+def log():
     d = request.form
-    return d
+    return readCSV.login(d['user'], d['pass'])
+
+@app.route("/membership/", methods = ['POST'])
+def reg():
+    d = request.form
+    return readCSV.register(d['user'], d['pass'])
+
 if __name__ == '__main__':
     app.debug = True
     app.run()
